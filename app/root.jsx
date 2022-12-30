0b26d8d472990bd380d2c23babe75e0151167b1d
Bryan Cusme <bryan.cusme@devoteam.com>
2022-12-30 13:35:43 +0100

    remix set up

---
+++ app/root.jsx
@@ -0,0 +1,36 @@
+import {Meta, Links} from '@remix-run/react'
+import styles from './styles/index.css'
+export function meta(){
+    return(
+        {
+            charset: 'utf-8',
+            title: 'AF Store - Home',
+            viewport:'width=device-width, initial-scale=1'
+        }
+    )
+}
+export function links(){
+    return{
+        rel:'stylesheet',
+        href: styles
+    }
+
+}
+export default function App(){
+    return(
+        <Document><h1>Hello Remix World</h1></Document>
+    )
+}
+function Document({children}){
+    return(
+        <html lang="en">
+            <head>
+                <Meta/>   
+                <Links/>         
+            </head>
+            <body>
+                {children}
+            </body>
+        </html>
+    )
+}
