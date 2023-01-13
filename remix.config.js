9edbc96208de9086458e4446f57fa9ac3422f703
Bryan Cusme <bryan.cusme@devoteam.com>
2023-01-13 11:13:29 +0100

    fix: vercel deploy server file

--- remix.config.js
+++ remix.config.js
@@ -1,8 +1,14 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
+  serverBuildTarget: "vercel",
+  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
   // publicPath: "/build/",
+  cacheDirectory: './node_modules/.cache/remix',
+
+  future: {},
+
 };
